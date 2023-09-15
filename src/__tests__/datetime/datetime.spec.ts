857541cb74898f6b7a067e7225d6a0674e00d86f
Antonino Piazza <antonino.piazza@alludo.com>
2023-09-15 09:30:40 +0200

    fix: Hold to reset no longer working #20

--- src/__tests__/datetime/datetime.spec.ts
+++ src/__tests__/datetime/datetime.spec.ts
@@ -35,12 +35,12 @@ describe("datetime", () => {
         const hass = { states: { test: { attributes: { friendly_name: "friendly name" } } } } as any;
         const entity = { id: "test" } as any;
         const element = document.createElement("ha-call-service-button") as any;
-        element.buttonTapped = jest.fn();
+        element._buttonTapped = jest.fn();
         setDatetimeServiceFactoryMock.mockReturnValue(element);
 
         resetDate($event, hass, entity);
 
         expect(setDatetimeServiceFactoryMock).toBeCalledWith(hass, "Do you want to reset friendly name?", "test", expect.anything());
-        expect(element.buttonTapped).toBeCalled();
+        expect(element._buttonTapped).toBeCalled();
     });
 });

1e580ebcd09c7e49c1d49a371d14687bb5a3822d
Antonino Piazza <antpza@gmail.com>
2024-12-27 07:57:47 +0100

    Fix DatetimeIcon

--- src/datetime/DatetimeIcon.svelte
+++ src/datetime/DatetimeIcon.svelte
@@ -5,21 +5,26 @@
     import {getState, isExpired, resetDate} from "./datetime";
     import {hold} from "../actions/hold";
 
-    export let entity: IEntity = undefined;
-    export let hass: IHass = undefined;
-    export let resetforward: boolean;
+    type Props = {
+        entity: IEntity;
+        hass: IHass;
+        resetForward: boolean;
+    };
 
-    $: icon = hass?.states?.[entity?.id]?.attributes?.icon;
-    $: color = isExpired(max, resetforward, state) ? "#df4c1e" : "#44739e";
-    $: max = entity?.max;
-    $: state = getState(hass, entity);
-    $: title = hass?.states?.[entity?.id]?.attributes?.friendly_name;
+    let { entity, hass, resetForward}  = $props<Props>();
+
+    let max = $derived<number>(entity?.max);
+    let state = $derived<number>(getState(hass, entity));
+    let icon = $derived<string>(hass?.states?.[entity?.id]?.attributes?.icon);
+    let color = $derived<"#df4c1e" | "#44739e">(isExpired(max, resetForward, state) ? "#df4c1e" : "#44739e");
+    let title = $derived<string>(hass?.states?.[entity?.id]?.attributes?.friendly_name);
 </script>
 
 <ha-icon data-testid="icon" {icon} style:color
          title="hold to reset {title}"
          use:hold
-         on:hold={(event) => resetDate(entity, event, hass, resetforward ? 1 : 0)}></ha-icon>
+         onhold={(event) => resetDate(entity, event, hass, resetForward ? 1 : 0)}>
+</ha-icon>
 
 <style>
 </style>
